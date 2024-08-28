f75c6231f93c9a94e244a0f916f23dadca946359
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-27 19:27:10 -0500

    feat: add effect to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import type { User, UserList } from './types';
 
 function App(): JSX.Element {
+  const [usersList, setUsersList] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+      .then((result) => result.json())
+      .then((data: UserList) => setUsersList(data.results));
+  }, []);
 
   return (
     <>
       <h1>Prueba Tecnica</h1>
+      <pre>{JSON.stringify(usersList, null, 2)}</pre>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
